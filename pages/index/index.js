d28ba21ea9c884679eca3b9d039c6a4239f5a42c
Niu <oncwnuBp78B7SdcA-loQ8kJKCrGo@git.weixin.qq.com>
2023-03-02 17:58:22 +0800

    share1.5

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,21 +1,221 @@
 // index.js
+const app = getApp()
+const chooseLocation = requirePlugin('chooseLocation');
+var COS = require('../../libs/cos-wx-sdk-v5.js')
+var util = require('../../libs/util.js')
 Page({
   data: {
-    item: 0,
     tab: 0,
-    location:"定位",
+    tabs: 0,
+    item: 0,
+    l_id: 0,
+    location: "定位",
+    time: '',
+    chooseLocation: "", //位置
+    banner:'',
+    list: [{
+        id: 0,
+        avatar: '/images/shouye.png',
+        nickName: 'MR.D',
+        time: '2023年3月1日',
+        location: '新乡学院D10',
+        distance: '100M',
+        content: "DIY扩展屏，由14寸笔记本屏幕、主板、充电宝、微积木DIY拼成。",
+        imgBox: ["https://img.vinua.cn/images/nSBX.jpeg", "https://img.vinua.cn/images/nE0b.jpeg"],
+        price: '188',
+        is_shouCang: true,
+
+      },
+      {
+        id: 1,
+        avatar: '/images/shouye.png',
+        nickName: 'MR.D',
+        time: '2023年3月1日',
+        location: '新乡学院D10',
+        distance: '100M',
+        content: "DIY扩展屏，由14寸笔记本屏幕、主板、充电宝、微积木DIY拼成。",
+        imgBox: ['/images/home.png', '/images/pengyouquan.png'],
+        price: '188',
+        is_shouCang: false,
+
+      }
+    ]
   },
-  changeItem: function (e) {
-    console.log(e);
+  changeTab: function (e) {
+    // console.log(e);
+    this.setData({
+      tab: e.target.dataset.item
+    })
+  },
+  changeTabs: function (e) {
+    // console.log(e);
+    this.setData({
+      tabs: e.detail.current
+    })
+  },
+  changeItem(e) {
+    // console.log(e);
     this.setData({
       item: e.target.dataset.item
     })
   },
-  changeTab: function (e) {   
-    console.log(e);
-    this.setData({
-      tab: e.detail.current
+  search(e) {
+    wx.showToast({
+      title: '搜索' + e.detail.value,
     })
   },
+  add() {
+    // console.log('1');
+    wx.reLaunch({
+      url: '/pages/add/add',
+    })
+  },
+  is_shouCang(e) {
+    var i = e.target.dataset.item
+    var is = this.data.list[i].is_shouCang
+    var liNo = this.data.list[i]
+    console.log(is);
+    is = !is
+    console.log(this.data.list[i].is_shouCang);
+  },
+  time() {
+    var today = new Date()
+    var YY = today.getFullYear()
+    var MM = today.getMonth() + 1
+    var DD = today.getDate()
+    var RR = today.getDay()
+    var hh = today.getHours()
+    var mm = today.getMinutes()
+    var ss = today.getSeconds()
+    var time = YY + '年' + MM + '月' + DD + '日'
+    this.setData({
+      time: time
+    })
+    // console.log(time);
+  },
+  location(){
+    // 获取用户地理位置
+    wx.getLocation({
+      type: 'wgs84',
+      success (res) {
+        const latitude = res.latitude
+        const longitude = res.longitude
+        const speed = res.speed
+        const accuracy = res.accuracy
+        console.log(res);
+      },
+     })
+  },
+  // 微信获取地理位置 选择位置  授权位置信息
+  getUserLocation: function () {
+    let vm = this;
+    wx.getSetting({
+        success: (res) => {
+            console.log(JSON.stringify(res))
+            // res.authSetting['scope.userLocation'] == undefined    表示 初始化进入该页面
+            // res.authSetting['scope.userLocation'] == false    表示 非初始化进入该页面,且未授权
+            // res.authSetting['scope.userLocation'] == true    表示 地理位置授权
+            if (res.authSetting['scope.userLocation'] != undefined && res.authSetting['scope.userLocation'] != true) {
+                wx.showModal({
+                    title: '请求授权当前位置',
+                    content: '需要获取您的地理位置，请确认授权',
+                    success: function (res) {
+                        if (res.cancel) {
+                            wx.showToast({
+                                title: '拒绝授权',
+                                icon: 'none',
+                                duration: 1000
+                            })
+                        } else if (res.confirm) {
+                            wx.openSetting({
+                                success: function (dataAu) {
+                                    if (dataAu.authSetting["scope.userLocation"] == true) {
+                                        wx.showToast({
+                                            title: '授权成功',
+                                            icon: 'success',
+                                            duration: 1000
+                                        })
+                                        //再次授权，调用wx.getLocation的API
+                                        vm.getLocation();
+                                    } else {
+                                        wx.showToast({
+                                            title: '授权失败',
+                                            icon: 'none',
+                                            duration: 1000
+                                        })
+                                    }
+                                }
+                            })
+                        }
+                    }
+                })
+            } else if (res.authSetting['scope.userLocation'] == undefined) {
+                //调用wx.getLocation的API
+                vm.getLocation();
+            } else {
+                //调用wx.getLocation的API
+                vm.getLocation();
+            }
+        }
+    })
+},
 
+// 微信选择位置 调用API 定位当前位置 获取坐标
+getLocation: function () {
+    // const key = 'YPJBZ-3VICP-OYWDV-VQDUT-FCI7J-MPFYK'; //使用在腾讯位置服务申请的key
+    // const referer = 'wx789e5aabeb07bfef'; //调用插件的app的名称
+    const key = 'PMWBZ-KDRLX-H3C4C-ZAH36-WB2YT-GYBN5'; //使用在腾讯位置服务申请的key
+    const referer = 'wx6d3c8ce12b2a4f0c'; //调用插件的app的名称
+    wx.navigateTo({
+        url: 'plugin://chooseLocation/index?key=' + key + '&referer=' + referer
+    });
+
+    //选择地址
+    //  let that = this;
+    //  wx.chooseLocation({
+    //   success: function(res) {
+    //     console.info(res);
+    //     that.setData({
+    //       chooseLocation: res,
+    //     })
+    //   },
+    // })
+},
+banner(){
+  var that = this
+  wx.request({
+    url: app.globalData.serverApi +'/selectAllBanner',
+    method:'POST',
+    success(res){
+      console.log('lunbo:',res.data.response);
+      that.setData({
+        banner:res.data.response
+      })
+    }
+  })
+},
+
+  onLoad() {
+    this.time()
+    this.location()
+  },
+  onShow(){
+    // 从地图选点插件返回后，在页面的onShow生命周期函数中能够调用插件接口，取得选点结果对象
+    let location = chooseLocation.getLocation(); // 如果点击确认选点按钮，则返回选点结果对象，否则返回null
+    console.log("location133:", location)
+    // console.log("location144:", location.name)
+    let that = this;
+    // JSON.stringify(location)
+    // JSON.parse(location)
+    this.setData({
+        chooseLocation: location,
+    })
+    if(location){
+      this.setData({
+      location:location.district
+    })
+    }
+    this.banner()
+    
+  }
 })
--- pages/map/map.js
+++ pages/map/map.js
@@ -1,201 +1,142 @@
-// // pages/map/map.js
 // var QQMapWX = require('../../libs/qqmap-wx-jssdk.min.js')
 const app = getApp()
 Page({
   data: {
-    // longitude: 108.07332649827005, //默认定位经度
-    // latitude: 34.28626496061992, //默认定位纬度
     latitude: 35.30351, //默认定位纬度
     longitude: 113.87523, //默认定位经度
     markers: [
-      //八教垃圾桶位置
-      {
-        id: 0,
-        iconPath: "/images/home(1).png",
-        latitude: 34.28594472914285,
-        longitude: 108.07340294122699,
-        width: 20, //图片显示宽度
-        height: 20, //图片显示高度
-        title: "牛亚博",
-        alpha: 1 //透明度 0-1 
-      },
-      //三教垃圾桶位置
-      {
-        id: 1,
-        iconPath: "/images/delect.png",
-        latitude: 34.28345098172088,
-        longitude: 108.07423643767835,
-        width: 20,
-        height: 20,
-        callout: {
-          // color:'#ffffff',
-          content: '气泡内容',
-          fontSize: 12,
-          borderRadius: 5,
-          borderWidth: 1,
-          bgColor: '#FFFFFFcc',
-          padding: 5,
-          textAlign: 'center',
-          display: "ALWAYS",
-          alpha: 0.1,
-        },
-        // alpha: 0.1 //透明度 0-1 
-      },
-      //北秀垃圾桶位置
-      {
-        id: 2,
-        iconPath: "/images/delect.png",
-        latitude: 34.28520896777005,
-        longitude: 108.0694815516472,
-        width: 20,
-        height: 20,
-        // title: "牛亚博",
-        label: {
-          content: "999"
-        }
-      },
-      //信工楼垃圾桶位置
-      {
-        id: 3,
-        iconPath: "/images/delect.png",
-        latitude: 34.2842427171466,
-        longitude: 108.0724158883095,
-        width: 20,
-        height: 20,
-        title: "牛亚博",
-      },
-      //10号寝室楼的位置
-      {
-        id: 4,
-        iconPath: "/images/delect.png",
-        latitude: 34.286067170734036,
-        longitude: 108.0664473026991,
-        width: 20,
-        height: 20,
-        title: "牛亚博",
-      },
-      //14号寝室楼的位置 
-      {
-        id: 5,
-        iconPath: "/images/delect.png",
-        latitude: 34.287375788724745,
-        longitude: 108.06752823293209,
-        width: 20,
-        height: 20,
-        title: "牛亚博",
-      },
-      //理学院垃圾桶位置
-      {
-        id: 6,
-        iconPath: "/images/delect.png",
-        latitude: 34.28801845563627,
-        longitude: 108.07408086955549,
-        width: 20,
-        height: 20,
-        title: "牛亚博",
-      },
-      //食品院垃圾桶位置
-      {
-        id: 7,
-        iconPath: "/images/delect.png",
-        latitude: 34.288367488192435,
-        longitude: 108.07558692991735,
-        width: 20,
-        height: 20,
-        title: "牛亚博",
-      },
-      //动科楼垃圾桶位置
-      {
-        id: 8,
-        iconPath: "/images/delect.png",
-        latitude: 34.28487044889044,
-        longitude: 108.07326279580593,
-        width: 20,
-        height: 20,
-        title: "牛亚博",
-      }
+      // {
+      //   id: 0,
+      //   iconPath: "/images/home(1).png",
+      //   latitude: 34.28594472914285,
+      //   longitude: 108.07340294122699,
+      //   width: 20, //图片显示宽度
+      //   height: 20, //图片显示高度
+      //   title: "牛亚博",
+      //   alpha: 1 //透明度 0-1 
+      // },
+      // {
+      //   id: 1,
+      //   iconPath: "/images/delect.png",
+      //   latitude: 34.28345098172088,
+      //   longitude: 108.07423643767835,
+      //   width: 20,
+      //   height: 20,
+      //   callout: {
+      //     // color:'#ffffff',
+      //     content: '气泡内容',
+      //     fontSize: 12,
+      //     borderRadius: 5,
+      //     borderWidth: 1,
+      //     bgColor: '#FFFFFFcc',
+      //     padding: 5,
+      //     textAlign: 'center',
+      //     display: "ALWAYS",
+      //     alpha: 0.1,
+      //   },
+      //   // alpha: 0.1 //透明度 0-1 
+      // },
+      // {
+      //   id: 2,
+      //   iconPath: "/images/delect.png",
+      //   latitude: 34.28520896777005,
+      //   longitude: 108.0694815516472,
+      //   width: 20,
+      //   height: 20,
+      //   // title: "牛亚博",
+      //   label: {
+      //     content: "999"
+      //   }
+      // },
     ]
+  },
+  // 微信获取地理位置 选择位置  授权位置信息
+  getUserLocation: function () {
+    let vm = this;
+    wx.getSetting({
+      success: (res) => {
+        console.log(JSON.stringify(res))
+        // res.authSetting['scope.userLocation'] == undefined    表示 初始化进入该页面
+        // res.authSetting['scope.userLocation'] == false    表示 非初始化进入该页面,且未授权
+        // res.authSetting['scope.userLocation'] == true    表示 地理位置授权
+        if (res.authSetting['scope.userLocation'] != undefined && res.authSetting['scope.userLocation'] != true) {
+          wx.showModal({
+            title: '请求授权当前位置',
+            content: '需要获取您的地理位置，请确认授权',
+            success: function (res) {
+              if (res.cancel) {
+                wx.showToast({
+                  title: '拒绝授权',
+                  icon: 'none',
+                  duration: 1000
+                })
+              } else if (res.confirm) {
+                wx.openSetting({
+                  success: function (dataAu) {
+                    if (dataAu.authSetting["scope.userLocation"] == true) {
+                      wx.showToast({
+                        title: '授权成功',
+                        icon: 'success',
+                        duration: 1000
+                      })
+                      //再次授权，调用wx.getLocation的API
+                      vm.getLocation();
+                    } else {
+                      wx.showToast({
+                        title: '授权失败',
+                        icon: 'none',
+                        duration: 1000
+                      })
+                    }
+                  }
+                })
+              }
+            }
+          })
+        } else if (res.authSetting['scope.userLocation'] == undefined) {
+          //调用wx.getLocation的API
+          // vm.getLocation();
+        } else {
+          //调用wx.getLocation的API
+          // vm.getLocation();
+        }
+      }
+    })
   },
 
   // 获取当前位置定位
   onLoad: function () {
-    var that = this;
-    wx.getLocation({
-      type: "wgs84",
-      success: function (res) {
-        // var latitude = res.latitude;
-        // var longitude = res.longitude;
-        console.log(res);
-        that.setData({
-          latitude: res.latitude,
-          longitude: res.longitude,
-          // markers: [{
-          //   latitude: res.latitude,
-          //   longitude: res.longitude
-          // }]
-        })
-      }
-    })
-  },
-  // 获取附近的共享
-  // 周边搜索（圆形范围）
-  onShow: function () {
     var that = this
+    that.getUserLocation()
     that.selectMarket()
-
-    // var location = that.data.latitude + "," + that.data.longitude
-    // console.log(location)
-    // wx.request({
-    //   url: 'https://apis.map.qq.com/place_cloud/search/nearby',
-    //   // url:'&filter=x.price>18 and x.price<=20 and x.star>=3&orderby=x.price desc',
-    //   method: 'GET',
-    //   data: {
-    //     table_id: '0o_YuzfKv09CC36Lv1',
-    //     key: 'PMWBZ-KDRLX-H3C4C-ZAH36-WB2YT-GYBN5',
-    //     // location: '35.30351,113.87523',
-    //     location: location,
-    //     radius: 10000, //半径，单位：米，默认5,000米，最大支持10,000米
-    //     auto_extend: 1,
-    //     // keyword:"车",
-    //     // chooseLocation: JSON.stringify(that.data.chooseLocation == null ? "" : that.data.chooseLocation),
-    //   },
-    //   success: (res) => {
+    // getLocation
+    // wx.getFuzzyLocation({
+    //   type: "wgs84",
+    //   success: function (res) {
+    //     console.log("-----");
     //     console.log(res);
-    //     let list = res.data.result.data;
-    //     let arr = [];
-
-    //     list.forEach((item, list) => {
-    //       item.width = 30; //这个后端数据没反,就自己设置图标大小
-    //       item.height = 35; //这个后端数据没反,就自己设置图标大小
-    //       item.latitude = item.location.lat;
-    //       item.longitude = item.location.lng;
-    //       item.iconPath = '/images/delect.png';
-    //       item.callout = {
-    //           // color:'#ffffff',
-    //           content: '气泡内容',
-    //           fontSize: 12,
-    //           borderRadius: 5,
-    //           borderWidth: 1,
-    //           bgColor: '#FFFFFFcc',
-    //           // borderColor:"#rrrrrr",
-    //           padding: 5,
-    //           textAlign: 'center',
-    //           display: "ALWAYS",
-    //         },
-    //         arr.push(item)
-    //     })
-    //     let that = this
     //     that.setData({
-    //       markers: arr,
-    //     })
-    //     console.log(that.data.markers)
-    //   },
-    //   fail: res => {
-    //     wx.showToast({
-    //       title: "失败",
+    //       latitude: res.latitude,
+    //       longitude: res.longitude,
+    //       markers: [{
+    //         latitude: res.latitude,
+    //         longitude: res.longitude
+    //       }]
     //     })
     //   }
     // })
+  },
+  // 获取附近的共享
+
+
+
+  onShow: function () {
+    var that = this
+    // that.selectMarket()
+
+    var location = that.data.latitude + "," + that.data.longitude
+    console.log(location)
+
   },
 
   // 地图显示附近共享
@@ -211,7 +152,7 @@ Page({
       method: 'POST',
       data: {
         wxOpenId: userInfo.wxOpenId,
-        CategoryType:"1",
+        CategoryType: "1",
         title: "",
         publictiy: 1
       },
@@ -224,34 +165,36 @@ Page({
         if (res.data.code == 1) {
           // 如果没有查询出数据 load改变为已完成没有下一页了。
           if (res.data.response.content == 0) {
-            // wx.hideLoading()
-            // this.setData({
-            //     load: false
-            // });
             return
           }
+          var list = res.data.response.content
           let arr = [];
-          res.data.response.content.forEach(mar => {
+          // var item = {};
+
+          list.forEach((mar, list) => {
             // let d = new Date(item.target.create_time).getTime();
             // item.target.create_time = this.commentTimeHandle(d);
             // console.log(item.target.create_time)
             // if (item.target.images != "") {
             //   item.target.images = item.target.images.split(",");
             // }
-            var item = [];
-            item.id = mar.id,
+            var item = {};
+            item.id = mar.id;
             item.width = 30; //这个后端数据没反,就自己设置图标大小
             item.height = 35; //这个后端数据没反,就自己设置图标大小
-            item.latitude = mar.latitude;
-            item.longitude = mar.longitude;
-            item.iconPath = '/images/delect.png';
+            item.latitude = mar.target.latitude;
+            item.longitude = mar.target.longitude;
+            // item.iconPath = '/images/delect.png';
+            item.iconPath = mar.target.avatar_url;
             item.callout = {
                 // color:'#ffffff',
-                content: mar.title,
+                color: '#FF0000 ',
+                content: (mar.target.title.length > 10 ? mar.target.title.substr(0, 10) + "..." : mar.target.title) + "￥:" + mar.target.pirce,
                 fontSize: 12,
                 borderRadius: 5,
                 borderWidth: 1,
                 bgColor: '#FFFFFFcc',
+                width: 1,
                 // borderColor:"#rrrrrr",
                 padding: 5,
                 textAlign: 'center',
@@ -259,22 +202,32 @@ Page({
               },
               arr.push(item)
           })
-          console.log(this.data.markers)
-
-          console.log("---------")
-          console.log(arr)
-    var that = this
-
+          let that = this
           that.setData({
-            // circle_of_marget: that.data.circle_of_marget.concat(res.data.response.list),
-            // pageSize: res.data.response.pageSize,
-            // pageIndex: res.data.response.pageIndex,
             markers: arr,
-          });
-          console.log("---------")
-          console.log(this.data.markers)
-        }
+          })
 
+          // that.setData({
+          //   mapCtx: wx.createMapContext('map')
+          // })
+          // let {
+          //   mapCtx
+          // } = this.data
+          // let points = []
+          // // result.forEach(item => {
+          // //   let obj = {
+          // //     latitude: item.lat,
+          // //     longitude: item.lng
+          // //   }
+          // //   points.push(obj)
+          // // })
+          // mapCtx.includePoints({
+          //   padding: [10, , 10000, 100000],
+          //   // points,
+          //   arr,
+          // })
+
+        }
       },
       fail: res => {
         wx.showToast({
@@ -371,4 +324,60 @@ Page({
       }
     })
   },
+  // 周边搜索（圆形范围）
+  shousuo: function () {
+    // wx.request({
+    //   url: 'https://apis.map.qq.com/place_cloud/search/nearby',
+    //   // url:'&filter=x.price>18 and x.price<=20 and x.star>=3&orderby=x.price desc',
+    //   method: 'GET',
+    //   data: {
+    //     table_id: '0o_YuzfKv09CC36Lv1',
+    //     key: 'PMWBZ-KDRLX-H3C4C-ZAH36-WB2YT-GYBN5',
+    //     // location: '35.30351,113.87523',
+    //     location: location,
+    //     radius: 10000, //半径，单位：米，默认5,000米，最大支持10,000米
+    //     auto_extend: 1,
+    //     // keyword:"车",
+    //     // chooseLocation: JSON.stringify(that.data.chooseLocation == null ? "" : that.data.chooseLocation),
+    //   },
+    //   success: (res) => {
+    //     console.log(res);
+    //     let list = res.data.result.data;
+    //     let arr = [];
+
+    //     list.forEach((item, list) => {
+    //       item.width = 30; //这个后端数据没反,就自己设置图标大小
+    //       item.height = 35; //这个后端数据没反,就自己设置图标大小
+    //       item.latitude = item.location.lat;
+    //       item.longitude = item.location.lng;
+    //       item.iconPath = '/images/delect.png';
+    //       item.callout = {
+    //           // color:'#ffffff',
+    //           content: '气泡内容',
+    //           fontSize: 12,
+    //           borderRadius: 5,
+    //           borderWidth: 1,
+    //           bgColor: '#FFFFFFcc',
+    //           // borderColor:"#rrrrrr",
+    //           padding: 5,
+    //           textAlign: 'center',
+    //           display: "ALWAYS",
+    //         },
+    //         arr.push(item)
+    //     })
+    //     let that = this
+    //     that.setData({
+    //       markers: arr,
+    //     })
+    //     console.log(that.data.markers)
+    //   },
+    //   fail: res => {
+    //     wx.showToast({
+    //       title: "失败",
+    //     })
+    //   }
+    // })
+  },
+
+
 })
